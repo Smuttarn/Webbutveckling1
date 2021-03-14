4d3b264f4f67fce07c0021affe5d24dad01adfb9
Pablo Ahava Pizarro <66680173+Smuttarn@users.noreply.github.com>
2021-03-14 20:35:59 +0100

    Number, globalThis, not done Regex

--- 3.1.1/script.js
+++ 3.1.1/script.js
@@ -1,12 +1,18 @@
 var array = ["1", "2", "3", "4", "5"];
-var temp = false;
+var arrayBool = false;
 var value = Math.PI;
 //Button functions
 document.getElementById("arrayButton").onclick = function() {showArray()};
 document.getElementById("boolButton").onclick = function() {changeBool()};
 document.getElementById("dateButton").onclick = function() {displayDate()};
+document.getElementById("dateNumberButton").onclick = function() {displayDateNumbers()};
+
+document.getElementById("showGlobal").onclick = function() {showGlobal()};
 
 document.getElementById("mathStuff").onchange = function() {multiplyPi()};
+document.getElementById("switchWords").onkeyup = function() {updateWords()};
+document.getElementById("switchWords").onchange = function() {switchWords()};
+
 
 window.onload = function() {
 
@@ -22,12 +28,12 @@ function showArray(){
 function changeBool(){
     var text;
 
-    if(temp){
-        temp = false;
+    if(arrayBool){
+        arrayBool = false;
         text = "false";
     }
-    else if(!temp){
-        temp = true;
+    else if(!arrayBool){
+        arrayBool = true;
         text = "true";
     }
     document.getElementById("thetruth").innerHTML = text;
@@ -38,6 +44,11 @@ function displayDate(){
     document.getElementById("thedate").innerHTML = date;
 }
 
+function displayDateNumbers(){
+    var date = new Date();
+    document.getElementById("thedate").innerHTML = Number(date);
+}
+
 function multiplyPi(){
     var output;
 
@@ -46,5 +57,20 @@ function multiplyPi(){
 
     document.getElementById("mathOutput").innerHTML = output;
     document.getElementById("mathStuff").value = "";
+}
 
+function showGlobal(){
+    document.getElementById("globalOutput").innerHTML = globalThis.location.toString();
+}
+
+function updateWords(){
+    var text = document.getElementById("switchWords").value;
+    document.getElementById("switchOutput").innerHTML = "Input: \"" + text + "\"";
+}
+
+function switchWords(){
+    var replace = /(\w+)\s(\w+)/;
+    var txt = document.getElementById("switchOutput").innerHTML.value;
+    var newText = txt.replace(replace, "$2, $1");
+    document.getElementById("switchOutput").innerHTML = newText;
 }
